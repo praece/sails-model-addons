2258b4f3153a0607196973652b99a27d0aac0f05
Kevin OBrien <kevin.obrien@praece.com>
2015-12-31 14:38:57 -0800

    Added attribute types.

--- config/models.js
+++ config/models.js
@@ -1,4 +1,21 @@
+var is = require('is_js');
+
 module.exports.models = {
+	types: {
+    validateUnique: function(value, count) {
+      return count ? false : true;
+    },
+    isEmail: function(value) {
+      return is.email(value);
+    },
+    isPhone: function(value) {
+      return is.nanpPhone(value);
+    },
+    isZip: function(value) {
+      return is.usZipCode(value);
+    }
+  },
+
 	searchableFields: [],
 	search: require('../lib/search.js'),
 	searchCount: require('../lib/searchCount.js')
