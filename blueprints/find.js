1f5a4875ecdecfc56d3d4ab92b4d16d9ee3d7799
Chelsea Walker <chelzwa@gmail.com>
2016-01-20 16:08:30 -0800

    Fixed syntax error.

--- blueprints/find.js
+++ blueprints/find.js
@@ -53,7 +53,7 @@ module.exports = function findRecords (req, res) {
       return res.ok(records);
     })
     .catch(function(err) {
-      sails.log.error('Error during findRecords.', {error: err, Model.adapter.identity, where: where});
+      sails.log.error('Error during findRecords.', {error: err, model: Model.adapter.identity, where: where});
       
       return res.serverError(err);
     });
